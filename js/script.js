c33e74b660f13a4d2af17110850c9c2a6bbe48be
Ryan Janvier <ryanjanvier@Ryans-MacBook-Pro.local>
2019-05-08 13:29:37 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  // Scroll to anchor
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+  });
+});
+
+AOS.init({
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 400, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+});
